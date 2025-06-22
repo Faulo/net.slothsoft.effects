99e1a4be32fc3c0f9974c654d320faac25ed9912
Daniel Schulz <faulolio@gmail.com>
2025-06-22 14:17:12 +0200

    fix: asmdef

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -9,23 +9,12 @@ namespace Slothsoft.Effects {
     static class AssemblyInfo {
         public const string ID = "net.slothsoft.test-runner";
 
-        public const string NAMESPACE_RUNTIME = "Slothsoft.Events";
-        public const string NAMESPACE_EDITOR = "Slothsoft.Events.Editor";
+        public const string NAMESPACE_RUNTIME = "Slothsoft.Effects";
+        public const string NAMESPACE_EDITOR = "Slothsoft.Effects.Editor";
 
-        public const string NAMESPACE_TESTS_PLAYMODE = "Slothsoft.Events.Tests.PlayMode";
-        public const string NAMESPACE_TESTS_EDITMODE = "Slothsoft.Events.Tests.EditMode";
+        public const string NAMESPACE_TESTS_PLAYMODE = "Slothsoft.Effects.Tests.PlayMode";
+        public const string NAMESPACE_TESTS_EDITMODE = "Slothsoft.Effects.Tests.EditMode";
 
         public const string NAMESPACE_PROXYGEN = "DynamicProxyGenAssembly2";
-
-        public const string MENU = "Cursed Broom/";
-        public const string MENU_EFFECT = "Cursed Broom/Effect/";
-        public const string MENU_GAMESTATE = "Cursed Broom/Game State/";
-        public const string MENU_LEVEL = "Cursed Broom/Level/";
-        public const string MENU_LEVEL_TILE = "2D/Tiles/";
-        public const string MENU_AVATAR = "Cursed Broom/Avatar/";
-        public const string MENU_AVATAR_MOVEMENT = "Cursed Broom/Avatar/Movement/";
-        public const string MENU_STORY = "Cursed Broom/Story/";
-        public const string MENU_INPUT = "Cursed Broom/Input/";
-        public const string MENU_EVENTS = "Cursed Broom/Events/";
     }
 }
